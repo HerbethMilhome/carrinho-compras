b16828c3eaacb524657779185c3013db5579f9e0
Herbeth Milhome <h2milhome@gmail.com>
2025-08-14 21:40:42 -0300

    implementsd redis

--- src/main/java/dev/java/ecommerce/basketservice/services/ProductServece.java
+++ src/main/java/dev/java/ecommerce/basketservice/services/ProductServece.java
@@ -3,21 +3,28 @@ package dev.java.ecommerce.basketservice.services;
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductServece {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getALlProducts() {
+        log.info("Getting all products from Platzi Store...");
         return platziStoreClient.getAllProducts();
     }
 
+    @Cacheable(value = "product", key = "#id")
     public PlatziProductResponse getProductById(Long id) {
+        log.info("Getting product by id: {}", id);
         return platziStoreClient.getProductById(id);
     }
 
